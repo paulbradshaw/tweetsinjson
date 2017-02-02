344343047d2021541696075171d365549e27d266
Paul Bradshaw <paulonhismobile@gmail.com>
2017-02-02 19:39:32 +0000

    Add files via upload

---
+++ Chelt Tweet JSON clean.R
@@ -0,0 +1,15 @@
+#install packages (if not already installed)
+install.packages('jsonlite')
+install.packages('dplyr')
+#load required packages
+library(dplyr)
+library(jsonlite)
+#get data from Firebase REST API
+tweets <- fromJSON("https://jazzdayjhive.firebaseio.com/chelt2016/.json?print=pretty&auth=mCZbhnOZLSPzV1Ur3ia7BovAbxQAMoXuIGlJZ710")
+#convert json to data table, ogranised according to element 'user'
+data <- bind_rows(tweets, .id = 'user')
+#write to csv
+write.csv(data, 'chelt_tweets_sorted.csv')
+#resulting file has a few odd variables at the end, but otherwise looks ok.
+
+
