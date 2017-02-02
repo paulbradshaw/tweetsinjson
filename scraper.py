82d847b84677b26835ef09c8a29b134175808c1e
Paul Bradshaw <paulonhismobile@gmail.com>
2017-02-02 15:50:03 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,24 +1,56 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+#!/usr/bin/env python
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+#import our libraries
+import json
+import requests
+import urllib
+import scraperwiki
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+#some JSON files
+jsonurlgh = 'https://raw.githubusercontent.com/paulbradshaw/scraping-for-everyone/gh-pages/webpages/cheltenhamjazz-export.json'
+jsonurl = 'https://paulbradshaw.github.io/scraping-for-everyone/webpages/cheltenhamjazz-export.json'
+trumpjson = 'https://petition.parliament.uk/petitions/178844.json'
+
+#fetch the json from the URL
+response = urllib.urlopen(jsonurl)
+#load it into variable called x
+x = json.loads(response.read())
+#let's see what we have
+print x
+print len(x)
+#drill down a bit into the 'posts' branch which contains everything
+print x['posts']
+#store that in a new variable
+posts = x['posts']
+#how many items?
+print len(x['posts'])
+
+#each branch has a different ID number, so we need to grab those in order to then specify each branch in turn
+
+#this only grabs the ID number of each
+#create an empty list to store the ID numbers, which we can then loop through to grab each tweet
+postids = []
+for post in posts:
+    print post
+    postids.append(post)
+#create empty dict to store the data
+record = {}
+#loop through the codes
+for code in postids:
+    print x['posts'][code]
+    #test that we can grab the text
+    print x['posts'][code]['text']
+    #start storing each field in the dict
+    record['text'] = x['posts'][code]['text']
+    record['authorid'] = x['posts'][code]['author']
+    try:
+        record['imageurl'] = x['posts'][code]['image']
+    except KeyError:
+        record['imageurl'] = 'NULL'
+    record['lon'] = x['posts'][code]['lon']
+    record['lat'] = x['posts'][code]['lat']
+    record['timestamp'] = x['posts'][code]['timestamp']
+    #this is the tweet code that we are using
+    record['tweetid'] = code
+    record['fulljson'] = str(x['posts'][code])
+    scraperwiki.sql.save(['tweetid'], record)
